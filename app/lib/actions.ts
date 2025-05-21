d5c7ba6523189db6808fa9096228a61bbdf3bada
mdrennan91 <mitcheldrennan@gmail.com>
2025-05-21 08:13:52 -0700

    Chapter 13 + save progress

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,12 +26,17 @@ export async function createInvoice(formData: FormData) {
   // Test it out:
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
+  
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
  
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -49,11 +54,16 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
