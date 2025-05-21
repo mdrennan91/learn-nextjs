8605168cadc4c7121fa027bf9e1d757156d0987f
mdrennan91 <mitcheldrennan@gmail.com>
2025-05-21 09:21:28 -0700

    pnpm lint - error fixes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -60,6 +60,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    console.error(error);
     // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice.',
@@ -103,6 +104,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.error(error);
     return {
       message: 'Database Error: Failed to Update Invoice.',
     };
